7e815428abe9a15ed285146390ecc9056f0adf44
sunxiaoling <amylovesong.sun@gmail.com>
2014-07-03 16:55:36 +0800

    create Base64CoderCC based on Commons Codec API

--- MyUtil/src/com/sun/base64/TestBase64.java
+++ MyUtil/src/com/sun/base64/TestBase64.java
@@ -13,16 +13,27 @@ public class TestBase64 {
 		String data = "Ad3";
 
 		// use Bouncy Castle API
+		/*
+		 * try { // encode String encodedString = Base64Coder.encode(data);
+		 * System.out.println(encodedString); // decode String decodedString =
+		 * Base64Coder.decode(encodedString); System.out.println(decodedString);
+		 * } catch (UnsupportedEncodingException e) { e.printStackTrace(); }
+		 */
+
+		// use Commons Codec API
 		try {
 			// encode
-			String encodedString = Base64Coder.encode(data);
+			String encodedString = Base64CoderCC.encodeSafe(data);// 这种编码方式的输出会多一个换行符
 			System.out.println(encodedString);
-			// decode
-			String decodedString = Base64Coder.decode(encodedString);
+			String decodedString = Base64CoderCC.decode(encodedString);
+			System.out.println(decodedString);
+
+			encodedString = Base64CoderCC.encode(data);// 这种编码方式的输出没有换行符
+			System.out.println(encodedString);
+			decodedString = Base64CoderCC.decode(encodedString);
 			System.out.println(decodedString);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-
 	}
 }
