6c0f66d308e1302f0715ff857b8bb2c3b51692a6
sunxiaoling <amylovesong.sun@gmail.com>
2014-07-03 21:38:39 +0800

    create UrlBase64Coder

--- MyUtil/src/com/sun/base64/TestUrlBase64.java
+++ MyUtil/src/com/sun/base64/TestUrlBase64.java
@@ -3,12 +3,12 @@ package com.sun.base64;
 import java.io.UnsupportedEncodingException;
 
 /**
- * Base64 test code
+ * UrlBase64 test code
  * 
  * @author sxl
  * 
  */
-public class TestBase64 {
+public class TestUrlBase64 {
 	public static void main(String[] args) {
 		String data = "Ad31";
 
@@ -16,10 +16,10 @@ public class TestBase64 {
 		try {
 			System.out.println("Bouncy Castle:");
 			// encode
-			String encodedString = Base64Coder.encode(data);
+			String encodedString = UrlBase64Coder.encode(data);
 			System.out.println(encodedString);
 			// decode
-			String decodedString = Base64Coder.decode(encodedString);
+			String decodedString = UrlBase64Coder.decode(encodedString);
 			System.out.println(decodedString);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
@@ -29,17 +29,14 @@ public class TestBase64 {
 		try {
 			System.out.println("Commons Codec:");
 			// encode
-			String encodedString = Base64CoderCC.encodeSafe(data);// 这种编码方式的输出会多一个换行符
+			String encodedString = UrlBase64CoderCC.encode(data);
 			System.out.println(encodedString);
-			String decodedString = Base64CoderCC.decode(encodedString);
-			System.out.println(decodedString);
-
-			encodedString = Base64CoderCC.encode(data);// 这种编码方式的输出没有换行符
-			System.out.println(encodedString);
-			decodedString = Base64CoderCC.decode(encodedString);
+			// decode
+			String decodedString = UrlBase64CoderCC.decode(encodedString);
 			System.out.println(decodedString);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
+
 	}
 }
---
+++ MyUtil/src/com/sun/base64/UrlBase64CoderCC.java
@@ -0,0 +1,29 @@
+package com.sun.base64;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * UrlBase64 util class based on Commons Codec API
+ * 
+ * @author sxl
+ * 
+ */
+public class UrlBase64CoderCC {
+	public static final String ENCODING = "UTF-8";
+
+	// º”√‹
+	public static String encode(String data)
+			throws UnsupportedEncodingException {
+		byte[] b = Base64.encodeBase64URLSafe(data.getBytes(ENCODING));
+		return new String(b, ENCODING);
+	}
+
+	// Ω‚√‹
+	public static String decode(String data)
+			throws UnsupportedEncodingException {
+		byte[] b = Base64.decodeBase64(data.getBytes(ENCODING));
+		return new String(b, ENCODING);
+	}
+}
